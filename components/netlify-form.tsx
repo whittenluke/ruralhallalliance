044a2a4528319b6df3d97eb5437f47ca682bfef5
Luke Whitten <whittenluke@gmail.com>
2026-04-16 11:49:55 -0400

    hopefully fixed forms

--- components/netlify-form.tsx
+++ components/netlify-form.tsx
@@ -45,7 +45,7 @@ export function NetlifyForm({ name, formName, children }: NetlifyFormProps) {
     setErrorMessage(null);
     try {
       const body = formDataToUrlEncoded(form);
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body
@@ -69,7 +69,7 @@ export function NetlifyForm({ name, formName, children }: NetlifyFormProps) {
         <form
           name={name}
           method="POST"
-          action="/"
+          action="/__forms.html"
           data-netlify="true"
           netlify-honeypot="bot-field"
           onSubmit={handleSubmit}
